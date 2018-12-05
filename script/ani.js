51c970bb23a784dadb59467590219032e0c268ff
2018worker <45620913+2018worker@users.noreply.github.com>
2018-12-05 16:10:57 +0800

    Add files via upload

---
+++ script/ani.js
@@ -0,0 +1,80 @@
+var _body=document.getElementsByTagName("body")
+var jianjie=document.getElementsByClassName("jianjie");
+var cg=document.getElementById("change");
+var txt=document.getElementById("txt");
+var photos=document.getElementsByClassName("hdn");
+var lclick=document.getElementById("lclick");
+var rclick=document.getElementById("rclick");
+
+var timer;
+var num=0;
+var istra=true;
+
+jianjie[0].onmousemove=function(){
+	cg.style.opacity=0.3;
+	txt.style.opacity=1;
+
+}
+jianjie[0].onmouseout=function(){
+	cg.style.opacity=1;
+	txt.style.opacity=0;
+}
+
+_body[0].onload=function(){
+	timer=setInterval("ani()",1000);
+	photos[0].style.left=-800*(Math.floor(num/3))+"px";
+	num++;
+}
+
+function ani(){
+	num++;
+	//console.log(num);
+	if(num%3==0){
+		photos[0].style.transition="all 1s";
+		photos[0].style.left=-800*(Math.floor(num/3))+"px";
+	}
+	else if(num>=17){
+		num=2;
+		photos[0].style.transition=undefined;
+		photos[0].style.left=-800*(Math.floor(num/3))+"px";
+	}
+}
+
+rclick.onclick=function(){
+	if(num>=0&&num<=15){
+		num+=3;
+		photos[0].style.left=-800*(Math.floor(num/3))+"px";
+		num--;
+	}
+	else {
+		num=3;
+		photos[0].style.left=-800*(Math.floor(num/3))+"px";
+		num--;
+	}
+
+}
+lclick.onclick=function(){
+	if(num>=3&&num<=18){
+		num-=3;
+		photos[0].style.left=-800*(Math.floor(num/3))+"px";
+		num--;
+	}
+	else{
+		num=13;
+		photos[0].style.left=-800*(Math.floor(num/3))+"px";
+		num--;
+	}
+}
+
+document.onvisibilitychange=function(){   
+	if(document.visibilityState=="visible"){ 
+		      timer=setInterval("ani()", 1000);   
+	}
+	else{       
+		clearInterval(timer);   
+	}
+}
+
+
+
+
---
+++ script/phone.js
@@ -0,0 +1,55 @@
+var imgs=document.getElementsByClassName("imgshow");
+var pshow=document.getElementsByClassName("pshow");
+var namep=document.getElementsByClassName("namep")[0];
+var iname=new Array("白云仙馆","笨猪跳","鸿鹄楼","麓湖公园","聚芳园",
+					"五羊雕像","镇海楼","中山纪念碑","广州美术馆","海员亭",
+					"广州塔","海心沙","珠江夜游","白鹅潭","大沙头码头");
+
+switch(document.title){
+	case "view01":
+		//console.log(1);
+		innerBys();
+		break;
+	case "view02":
+		//console.log(2);
+		innerYxgy();
+		break;
+	case "view03":
+		//console.log(3);
+		innerYzj();
+		break;
+}
+
+function innerBys(){
+		for(let i=0;i<5;i++){
+		imgs[i].onclick=function(){
+			pshow[0].innerHTML="<div><img src=\"image\/bys\/b_view0"
+							 	+(i+1)+".jpg\" alt=\""+iname[i]+
+							 	"\" width=\"400px\" height=\"250px\"><\/div>";
+			namep.innerHTML=iname[i];
+		}
+	}
+}
+function innerYxgy(){
+		for(let i=0;i<5;i++){
+		imgs[i].onclick=function(){
+			pshow[0].innerHTML="<div><img src=\"image\/yxgy\/y_view0"
+							 	+(i+1)+".jpg\" alt=\""+iname[i+5]+
+							 	"\" width=\"400px\" height=\"250px\"><\/div>";
+			namep.innerHTML=iname[i+5];
+		}
+	}
+}
+function innerYzj(){
+		for(let i=0;i<5;i++){
+		imgs[i].onclick=function(){
+			pshow[0].innerHTML="<div><img src=\"image\/yzj\/z_view0"
+							 	+(i+1)+".jpg\" alt=\""+iname[i+10]+
+							 	"\" width=\"400px\" height=\"250px\"><\/div>";
+			namep.innerHTML=iname[i+10];
+		}
+	}
+}
+
+
+
